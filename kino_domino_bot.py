98598591b42c2a26c09d97fe3736ae210c4ac19b
Sabina Karaseva <Karasevasabina7266@yandex.ru>
2017-01-13 22:30:10 +0300

    Пробую кнопки. Пока без бд

--- kino_domino_bot.py
+++ kino_domino_bot.py
@@ -1,6 +1,6 @@
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters 
 from db import db_session, MovieTheaters, MetroStations, TimeSlots, Movies, MovieFormats
-#from telegram import ReplyKeyboardMarkup
+from telegram import ReplyKeyboardMarkup, KeyboardButton
 
 #markup = types.ReplyKeyboardMarkup()
 
@@ -9,33 +9,30 @@ def greet_user(bot, update):
 	bot.sendMessage(update.message.chat_id, text='Привет, друг! Что-то мне подсказывает, что ты хочешь сходить в кино :)  Какой фильм хочешь посмотреть?')
 
 
-#def location(bot, update):
- #   user = update.message.from_user
-   # user_location = update.message.location
-  #  update.message.reply_text('Спасибо')
-
-
-
 
 def show_error(bot, update, error):
     print('Update "{}" caused error "{}"'.format(update, error))
 
 def talk_to_me(bot, update):
     print("Пришло сообщение: " + update.message.text)
-    film_list = ["Неоновый демон", "Драйв", "Шерлок"]
+    film_list = ["Неоновый носочек", "Драйв", "Шерлок"]
     user_film = update.message.text
+
+    custom_keyboard = [[ user_film]] #['2'], ['3'], ['4']]
+    rm = ReplyKeyboardMarkup(custom_keyboard)
+
     if user_film in film_list:
-    	#bot.sendMessage(update.message.chat_id, reply_markup=ReplyKeyboardMarkup(user_film))
-
-
-    	bot.sendMessage(update.message.chat_id,  text='Угу, я тебя вроде понял и даже что-то нашел :) Выбери, пожалуйста, вариант фильма')
-    	bot.sendMessage(update.message.chat_id,  user_film)
+    	bot.sendMessage(update.message.chat_id,  text='Угу, я тебя вроде понял и даже что-то нашел :) Выбери, пожалуйста, вариант фильма', reply_markup=rm)
+    	 
 
     else:
     	bot.sendMessage(update.message.chat_id, text="Не найдено")
 
 
-
+#def location(bot, update):
+ #   user = update.message.from_user
+  #  user_location = update.message.location
+   # update.message.reply_text('')
 
 
 
