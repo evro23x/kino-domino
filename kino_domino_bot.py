0928bcb29060fa38992a0cb9f4d12f90a34194ae
Sabina <Karasevasabina@gmail.com>
2017-01-28 17:14:03 +0300

    добавлена поддержка нескольких пользователей одновременно

--- kino_domino_bot.py
+++ kino_domino_bot.py
@@ -5,8 +5,8 @@ from db_quering import get_movie_id, is_on_screen, find_closest_theater2, get_mo
 
 GET_A_MOVIE_NAME, ANALYZE_USER_LOCATION = range(2)
 
-USER_INPUT = ""
-USER_LOCATION = (0.0,0.0)
+USER_INPUT = {}
+USER_LOCATION = {}
 
 def show_error(bot, update, error):
     print('Update "{}" caused error "{}"'.format(update, error))
@@ -18,9 +18,11 @@ def greet_user(bot, update):
     return  GET_A_MOVIE_NAME
 
 
-def get_a_movie_name(bot, update): # может вернуть несколько id, в случае нескольких вариантов
+def get_a_movie_name(bot, update):  
     global USER_INPUT 
-    USER_INPUT = update.message.text 
+    chat_id = update.message.chat_id
+    USER_INPUT[chat_id] = update.message.text
+    print(USER_INPUT)
     location_keyboard = KeyboardButton(text="Найди кино рядом", request_location=True)
     custom_keyboard = [[ location_keyboard]]
     reply_markup = ReplyKeyboardMarkup(custom_keyboard)
@@ -28,6 +30,12 @@ def get_a_movie_name(bot, update): # может вернуть нескольк�
     return ANALYZE_USER_LOCATION
 
 
+
+#d= {1:3}
+#d[2] = 4
+#d = {1:3, 2:4}
+
+#USER_LOCATION{id:coordinates}
 
     
 """def ask_for_location(bot, update):
@@ -41,15 +49,14 @@ def get_a_movie_name(bot, update): # может вернуть нескольк�
 
 
 def analize_user_location(bot, update):
-    your_location = update.message.location
+    chat_id = update.message.chat_id
     global USER_LOCATION
-    USER_LOCATION = (your_location.latitude, your_location.longitude)
+    USER_LOCATION[chat_id] = (update.message.location.latitude, update.message.location.longitude)
     print(USER_LOCATION)
     print(USER_INPUT)
-    timetable = main_search(USER_INPUT, USER_LOCATION)
+    timetable = main_search(USER_INPUT[chat_id], USER_LOCATION[chat_id])
     print(timetable)
-    print('LOL')
-    bot.sendMessage(update.message.chat_id, timetable)
+    bot.sendMessage(update.message.chat_id, timetable, reply_markup=ReplyKeyboardRemove())
     #return GIVE_A_MOVIE_LIST
 
 
