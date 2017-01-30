9c7c688492bbbef10144d094cfd023a3190e6e1c
Sabina <Karasevasabina@gmail.com>
2017-01-30 18:14:31 +0300

    добавила кнопки выбора: смотреть дома или в кино

--- kino_domino_bot.py
+++ kino_domino_bot.py
@@ -1,9 +1,9 @@
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, ConversationHandler 
 #from db import db_session, MovieTheaters, MetroStations, TimeSlots, Movies, MovieFormats
 from telegram import ReplyKeyboardMarkup, KeyboardButton, ReplyKeyboardRemove #InlineQueryResult
-from db_quering import get_movie_id, is_on_screen, find_closest_theater2, get_movie_slots_in_theater_at_period, parse_time_table, main_search, UserRequestFail
+from db_quering import get_movie_id, find_closest_theater, get_movie_slots_in_theater_at_period, parse_time_table, main_search
 
-GET_A_MOVIE_NAME, ANALYZE_USER_LOCATION = range(2)
+GET_A_MOVIE_NAME, ANALYZE_USER_LOCATION, WHAT_TO_DO_NEXT = range(3)
 
 USER_INPUT = {}
 USER_LOCATION = {}
@@ -14,23 +14,29 @@ def show_error(bot, update, error):
 
 def greet_user(bot, update): 
     print('Вызван/ start')
-    bot.sendMessage(update.message.chat_id, text='Привет, друг! Что-то мне подсказывает, что ты хочешь сходить в кино :)  Какой фильм хочешь посмотреть?')
-    return  GET_A_MOVIE_NAME
+    variants = [['Посмотреть кино дома'], ['Сходить в кино']]
+    custom_keyboard = variants
+    rm = ReplyKeyboardMarkup(custom_keyboard)
+    bot.sendMessage(update.message.chat_id, text="Привет, друг! Чего бы тебе хотелось?", reply_markup=rm)
+     
 
-# def is_in_theatres(bot,update):
-#     movie_id = get_movie_id(update.message.text)
-#     if movie_id == None:
-#         bot.sendMessage(update.message.chat_id, text="Извини, но этот фильм сейчас не идет в кинотеатрах. Попробуй еще раз.")
-#         return GET_A_MOVIE_NAME
-#     return 
+    return  WHAT_TO_DO_NEXT
 
+
+def what_to_do_next(bot, update):
+    user_choice = [update.message.text]
+    variants = [['Посмотреть кино дома'], ['Сходить в кино']]
+    if user_choice == variants[1]:
+        bot.sendMessage(update.message.chat_id, text="Хорошо! Какой фильм хочешь посмотреть?", reply_markup=ReplyKeyboardRemove())
+        return  GET_A_MOVIE_NAME
+    else:
+        bot.sendMessage(update.message.chat_id, text="Здесь скоро будет ф-ция. А пока нажми /cancel и попробуй сходить в кино.")
 
 
 def get_a_movie_name(bot, update):  
     global USER_INPUT 
     chat_id = update.message.chat_id
     USER_INPUT[chat_id] = update.message.text
-
     movie_id = get_movie_id(update.message.text)
     if movie_id is None:
         bot.sendMessage(update.message.chat_id, text="Извини, но этот фильм сейчас не идет в кинотеатрах. Попробуй еще раз.")
@@ -46,25 +52,6 @@ def get_a_movie_name(bot, update):
 
 
 
-#d= {1:3}
-#d[2] = 4
-#d = {1:3, 2:4}
-
-#USER_LOCATION{id:coordinates}
-1. посмотреть в кино --- выдаем то, что уже написано
-2. посмотреть дома --- просим ввести название фильма, а мы выдаем похожие фильмы
-
-    
-"""def ask_for_location(bot, update):
-    location_keyboard = KeyboardButton(text="Найди кино рядом", request_location=True)
-    custom_keyboard = [[ location_keyboard]]
-    reply_markup = ReplyKeyboardMarkup(custom_keyboard)
-    bot.sendMessage(update.message.chat_id, reply_markup=reply_markup)
-
-    return ANALYZE_USER_LOCATION"""
-
-
-
 def analize_user_location(bot, update):
     chat_id = update.message.chat_id
     global USER_LOCATION
@@ -72,18 +59,10 @@ def analize_user_location(bot, update):
     print(USER_LOCATION)
     print(USER_INPUT)
     timetable = main_search(USER_INPUT[chat_id], USER_LOCATION[chat_id])
-    print(timetable)
-    bot.sendMessage(update.message.chat_id, timetable, reply_markup=ReplyKeyboardRemove())
-    #return GIVE_A_MOVIE_LIST
+    final_phrase = timetable + "Нажми /cancel, чтобы закончить"
+    bot.sendMessage(update.message.chat_id, final_phrase, reply_markup=ReplyKeyboardRemove())
 
-
-    
-"""def give_a_movie_list(bot, update):
-    timetable = main_search(USER_INPUT, USER_LOCATION)
-    print(timetable)
-    bot.sendMessage(update.message.chat_id, timetable)"""
-
-
+   
 
 def cancel(bot, update):
     bot.sendMessage(update.message.chat_id,'Пока! Надеюсь, я помог тебе!',
@@ -104,18 +83,11 @@ def main():
                  
                 GET_A_MOVIE_NAME: [MessageHandler([Filters.text], get_a_movie_name)],
 
-                #ASK_FOR_LOCATION: [MessageHandler([Filters.text], ask_for_location)],
+                ANALYZE_USER_LOCATION: [MessageHandler([Filters.location], analize_user_location)],
 
-                ANALYZE_USER_LOCATION: [MessageHandler([Filters.location], analize_user_location)]
+                WHAT_TO_DO_NEXT: [MessageHandler([Filters.text], what_to_do_next)]
 
-                # IS_IN_THEATRES: [MessageHandler([Filters.text], is_in_theatres)]
-
-                #GIVE_A_MOVIE_LIST: [MessageHandler([Filters.text], give_a_movie_list)],
-
-                #GIVE_A_MOVIE_LIST: [MessageHandler([Filters.location], give_a_movie_list)]
-            
-
-
+        
         },
 
         fallbacks=[CommandHandler('cancel', cancel)]
