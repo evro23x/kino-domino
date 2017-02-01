26fd695ed88028dd5c06950227408599d810bd6e
Evgeny Gurov <evro23x@mail.ru>
2017-02-01 17:48:01 +0300

    save code with debug info

--- parser_yandex.py
+++ parser_yandex.py
@@ -7,6 +7,8 @@ import requests
 import json
 
 ITERATIONS = 3
+DEBUG = False
+# DEBUG = True
 
 
 def get_or_create(current_session, model, **kwargs):
@@ -27,7 +29,7 @@ def get_json_from_url(url):
 def check_metro_in_db():
     metro_list = []
     for metro in get_json_from_url('https://api.hh.ru/metro/1')['lines']:
-        # print('Парсим все станции метро на ветке - {}'.format(metro['name']))
+        print('Парсим все станции метро на ветке - {}'.format(metro['name']))
         for station in metro['stations']:
             metro_from_db = get_or_create(db_session, MetroStations,
                                           title=station['name'],
@@ -68,7 +70,7 @@ def check_cinema_in_db(all_metro):
         # print(phones[2])
         # print('++++++++++++++++++++++++++++++++')
         # exit()
-        # print('Парсим всю инфу по кт - {}'.format(cinema['title']))
+        print('Парсим всю инфу по кт - {}'.format(cinema['title']))
         cinema_list.append(get_or_create(db_session, MovieTheaters,
                                          metro_id=metro_st_id,
                                          yandex_theater_id=cinema['id'],
@@ -117,11 +119,11 @@ def check_movie_in_db():
     url_new_movie_list = 'https://afisha.yandex.ru/api/events/actual?limit=12&offset=0&tag=cinema&hasMixed=0&' \
                          'filter=week-premiere&city=moscow'
     new_movie_list = get_json_from_url(url_new_movie_list)
-    # print(json.dumps(new_movie_list, sort_keys=True, indent=4, ensure_ascii=False))
 
     url_movie_list = 'https://afisha.yandex.ru/api/events/actual?' \
                      'limit=12&offset=0&tag=cinema&hasMixed=0&date=' + str(date.today()) + '&period=1&city=moscow'
     first_query_json = get_json_from_url(url_movie_list)
+
     movie_list = new_movie_list['data'] + first_query_json['data']
     movies_id = []
     for i in range(int(first_query_json['paging']['total'] / 12 + 1)):
@@ -132,7 +134,7 @@ def check_movie_in_db():
         movie_list = movie_list + get_json_from_url(url_movie_list)['data']
     for movie in movie_list:
         movies_id.append(movie['event']['id'])
-        # print('Парсим фильм {}'.format(movie['event']['title']))
+        print('Парсим фильм {}'.format(movie['event']['title']))
         get_or_create(db_session, Movies,
                       yandex_movie_id=movie['event']['id'],
                       title=movie['event']['title'],
@@ -143,78 +145,150 @@ def check_movie_in_db():
 
 def get_theater_id_by_yandex_id(yandex_theater_id):
     theater = db_session.query(MovieTheaters).filter_by(yandex_theater_id=yandex_theater_id).first()
-    return theater.id
+    return theater if theater else None
 
 
 def get_movie_id_by_yandex_id(yandex_movie_id):
-    theater = db_session.query(Movies).filter_by(yandex_movie_id=yandex_movie_id).first()
-    return theater.id
+    movie = db_session.query(Movies).filter_by(yandex_movie_id=yandex_movie_id).first()
+    return movie if movie else None
 
 
 def check_time_slot_in_db(movies_id):
-    counter = 0
-    # print('начало начал')
+    if DEBUG:
+        print('начало начал')
+    # q1 = 0
     for yandex_movie_id in movies_id:
-        # print('зашел в первый иф')
-        while counter < ITERATIONS:
-            # print('пришли в вайл')
+        # q1 += 1
+        # if q1 == 2:
+        #     exit()
+        movie = get_movie_id_by_yandex_id(yandex_movie_id)
+        print('Собираем расписание по фильму - {}'.format(movie.title))
+        # if yandex_movie_id == '5587e3c1cc1c7211b4ea81d3':
+        #     continue
+        if DEBUG:
+            print('начинаем разбирать фильм')
+        for counter in range(ITERATIONS):
+            if DEBUG:
+                print('пришли в вайл')
             session_date = date.today() + timedelta(counter)
-            counter += 1
+            print(session_date)
+
             header = 'https://afisha.yandex.ru/api/events/'
             date_params = '&date=' + str(session_date) + '&city=moscow'
-            test_url = header + yandex_movie_id + '/schedule_cinema?limit=10&offset=0' + date_params
-            # print(test_url)
+            first_query_url = header + yandex_movie_id + '/schedule_cinema?limit=10&offset=0' + date_params
+
+            first_query_result = get_json_from_url(first_query_url)
+
+            paging_count = first_query_result['schedule']['paging']['total']
+            # time_slot_list = first_query_result['schedule']['items']
+
+            # print(range(int(paging_count / 10 + 1)))
             # exit()
-            first_query_json = get_json_from_url(test_url)['schedule']
-            # print('выполнил запрос')
-            # print(json.dumps(first_query_json, sort_keys=True, indent=4, ensure_ascii=False))
-            # print(time_slot_list)
-            # exit()
-            if first_query_json['paging']['total'] == 0:
-                # print('проскочил в континуе')
+
+            # условие на случай если у фильма будет 0 сеансов за день(часто бывает у премьер)
+            if paging_count == 0:
+                if DEBUG:
+                    print('проскочил в континуе')
                 continue
-            for i in range(int(first_query_json['paging']['total'] / 10 + 1)):
-                # print('зашел в фор')
-                limit_offset = '/schedule_cinema?limit=10&offset=' + str(i * 10)
-                url = header + yandex_movie_id + limit_offset + date_params
-                # print('собрал второй урик')
 
-                time_slot_list = get_json_from_url(url)['schedule']
-                # print('выполнил второй запрос')
+            time_slot_list = []
+            # time_slot_list = [first_query_result['schedule']['items']]
+            # print(json.dumps(time_slot_list, sort_keys=True, indent=4, ensure_ascii=False))
+            # exit()
+
+            for i in range(int(paging_count / 10 + 1)):
                 # print(json.dumps(time_slot_list, sort_keys=True, indent=4, ensure_ascii=False))
-                # print(time_slot_list)
                 # exit()
+                if DEBUG:
+                    print('зашел в фор, страницы сеансов собираю ')
+                limit_offset = '/schedule_cinema?limit=10&offset=' + str(i * 10)
+                paging_time_slot_url = header + yandex_movie_id + limit_offset + date_params
 
-                for time_slot in time_slot_list['items']:
-                    theater_id_from_json = time_slot['place']['id']
-                    movie_format_from_json = time_slot['schedule']['format']['name']
-                    datetime_from_json = time_slot['schedule']['sessions']['datetime']
-                    max_price_from_json = time_slot['schedule']['sessions']['ticket']['price']['max']
-                    min_price_from_json = time_slot['schedule']['sessions']['ticket']['price']['min']
+                # print(paging_count)
+                # print(int(paging_count / 10 + 1))
+                # print(i)
+                # print(i*10)
+                # print(paging_time_slot_url)
+                # print('================================================================================')
 
-                    movie_format = get_or_create(db_session, MovieFormats, title=movie_format_from_json)
-                    print(get_theater_id_by_yandex_id(theater_id_from_json))
-                    print(get_movie_id_by_yandex_id(yandex_movie_id))
-                    print(movie_format.id)
-                    print(datetime.strptime(datetime_from_json, "%Y-%m-%dT%H:%M:%S"))
-                    print(max_price_from_json)
-                    print(min_price_from_json)
-                    print('++++++++++++++++++++++++++++++++')
-                    print('')
+                paging_time_slot_result = get_json_from_url(paging_time_slot_url)
 
-                    # get_or_create(db_session, TimeSlots,
-                    #               movie_theaters_id=get_theater_id_by_yandex_id(theater_id_from_json),
-                    #               movie_id=get_movie_id_by_yandex_id(yandex_movie_id),
-                    #               movie_formats_id=movie_format.id,
-                    #               time=datetime.strptime(datetime_from_json, "%Y-%m-%dT%H:%M:%S"),
-                    #               max_price=max_price_from_json,
-                    #               min_price=min_price_from_json)
+                for raw in paging_time_slot_result['schedule']['items']:
+                    time_slot_list.append(raw)
+
+            # print(json.dumps(time_slot_list, sort_keys=True, indent=4, ensure_ascii=False))
+            # print(json.dumps(get_json_from_url(url)['schedule'], sort_keys=True, indent=4, ensure_ascii=False))
+            # exit()
+
+            for time_slot in time_slot_list:
+                if DEBUG:
+                    print('зашел в фор')
+
+                # print(time_slot)
+                # exit()
+                theater_id_from_json = time_slot['place']['id']
+                theater = get_theater_id_by_yandex_id(theater_id_from_json)
+
+                # условие на случай если кинотеатра не будет в перечне кт(такое редко, но бывает)
+                if theater is None:
+                    continue
+
+                # if time_slot['schedule'][0]['sessions'][0]['ticket'] is not None:
+                #     print('if')
+                #     print(time_slot['schedule'][0]['sessions'][0]['ticket'])
+                #     print('++++++++++++++++++++++++++++++++')
+                #     print('')
+                # else:
+                #     print('else')
+                #     print(time_slot['schedule'][0]['sessions'])
+                #     print('++++++++++++++++++++++++++++++++')
+                #     print('')
+
+                # exit()
+                movie_format_from_json = time_slot['schedule'][0]['format']['name']
+                datetime_from_json = time_slot['schedule'][0]['sessions'][0]['datetime']
+                ticket = time_slot['schedule'][0]['sessions'][0]['ticket']
+                if ticket is None or ticket['price'] is None:
+                    max_price_from_json = 0
+                    min_price_from_json = 0
+                else:
+                    max_price_from_json = ticket['price']['max']
+                    min_price_from_json = ticket['price']['min']
+
+                movie_format = get_or_create(db_session, MovieFormats, title=movie_format_from_json)
+
+                # print(theater_id_from_json)
+                # print(theater_id)
+                # print(get_movie_id_by_yandex_id(yandex_movie_id))
+                # print(movie_format.id)
+                # print(datetime.strptime(datetime_from_json, "%Y-%m-%dT%H:%M:%S"))
+                # print(max_price_from_json)
+                # print(min_price_from_json)
+                # print('++++++++++++++++++++++++++++++++')
+                # print('')
+                # exit()
+                #
+
+                get_or_create(db_session, TimeSlots,
+                              movie_theaters_id=theater.id,
+                              movie_id=movie.id,
+                              movie_formats_id=movie_format.id,
+                              time=datetime.strptime(datetime_from_json, "%Y-%m-%dT%H:%M:%S"),
+                              max_price=max_price_from_json,
+                              min_price=min_price_from_json)
 
 
 def main():
+    # ww22 = '588a31b4cc1c72457c7e7ca8'
+    # ww33 = '554c5ecb179b116662abdb03'
+    # print(get_theater_id_by_yandex_id(ww22))
+    # print(get_theater_id_by_yandex_id(ww33))
+    # print(type(get_theater_id_by_yandex_id(ww22)))
+    # print(type(get_theater_id_by_yandex_id(ww33)))
+
     # pass
-    all_metro = check_metro_in_db()
-    check_cinema_in_db(all_metro)
+    # all_metro = check_metro_in_db()
+    # check_cinema_in_db(all_metro)
     movies_id_list = check_movie_in_db()
     check_time_slot_in_db(movies_id_list)
 
