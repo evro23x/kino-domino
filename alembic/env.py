c1e6cb603baa2027b83ff700bc2ac07ddeecda7f
Evgeny Gurov <evro23x@mail.ru>
2017-02-02 22:17:53 +0300

    edit alembic/env.py - add db_schema.py as config Base

--- alembic/env.py
+++ alembic/env.py
@@ -3,6 +3,8 @@ from alembic import context
 from sqlalchemy import engine_from_config, pool
 from logging.config import fileConfig
 
+from db_schema.py import Base
+
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
@@ -15,8 +17,7 @@ fileConfig(config.config_file_name)
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
-
+target_metadata = Base.metadata
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
 # my_important_option = config.get_main_option("my_important_option")
