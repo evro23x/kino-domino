5c85aa86930d0ba7f38e24ca1dce0d609bead689
Evgeny Gurov <evro23x@mail.ru>
2017-02-09 13:56:25 +0300

    movie rating bug - fix

--- parser_yandex.py
+++ parser_yandex.py
@@ -1,6 +1,5 @@
 from math import sqrt
 from bs4 import BeautifulSoup
-from urllib.request import urlopen
 from db_schema import db_session, MetroStations, MovieTheaters, TimeSlots, Movies, MovieFormats
 from datetime import datetime, date, time, timedelta
 import requests
@@ -10,6 +9,12 @@ ITERATIONS = 3
 
 
 def get_or_create(current_session, model, **kwargs):
+    """
+    Получить либо создать
+
+    Функция получения данных из базы по передаваемым параметрам.
+    Создает новую запись в базе если запрашиваемый отбъект отсуствует.
+    """
     instance = current_session.query(model).filter_by(**kwargs).first()
     if instance:
         return instance
@@ -21,10 +26,22 @@ def get_or_create(current_session, model, **kwargs):
 
 
 def get_json_from_url(url):
+    """
+    Получить json по ссылке
+
+    Обертка над получением данных в формате json по передаваемой ссылке
+    """
     return requests.get(url).json()
 
 
 def check_metro_in_db():
+    """
+    Актуализация перечня станций метро
+
+    Разбираем станции метро полученные от api hh и складываем в базу.
+    Информация о существующих станциях метро неизменна.
+    Количество станций может добавляться использую функцию get_or_create.
+    """
     metro_list = []
     for metro in get_json_from_url('https://api.hh.ru/metro/1')['lines']:
         print('Парсим все станции метро на ветке - {}'.format(metro['name']))
@@ -38,6 +55,15 @@ def check_metro_in_db():
 
 
 def check_cinema_in_db(all_metro):
+    """
+    Актуализация перечня кинотеатров
+
+    Разбираем список кинотеатров по Москве от afisha.yandex.ru .
+    Расчитываем ближаейшую станцию метро если она не указана в api и записываем данные к нам в базу
+    """
+    # TODO: Требуется доработать таким образом чтобы функция вызывалась при парсинге сеансов, так как там более
+    # удобная структура и оптимизация в количестве запросов, так как сюда уже будет передаваться list и не потребуется
+    # повторно вызывать get_json_from_url()
     cinema_list = []
     url_cinema_list = 'https://afisha.yandex.ru/api/events/cinema/places?limit=200&offset=0&city=moscow'
     for cinema in get_json_from_url(url_cinema_list)['items']:
@@ -73,6 +99,10 @@ def check_cinema_in_db(all_metro):
 
 
 def get_metro_stations_from_db():
+    """
+    Получение всех станций метро из базы.
+
+    """
     metro_list = []
     for u in db_session.query(MetroStations).all():
         result = {
@@ -86,18 +116,27 @@ def get_metro_stations_from_db():
 
 
 def get_closest_station(data, latitude, longitude):
+    """
+    Поиск ближайшей станции метро.
+    """
     return min(data, key=lambda data: get_distance(latitude, longitude, data['latitude'], data['longitude']))
 
 
 def get_distance(x1, y1, point_x1, point_y1):
+    """
+    Расчет расстояния между двумя точками.
+    """
     return sqrt((x1 - point_x1) ** 2 + (y1 - point_y1) ** 2)
 
 
 def check_movie_in_db():
-    # Комент для себя =) чтобы потом вспомнить как это работает.
-    # Собираем сначала список новых фильмов(премьерных) + первая страница всех остальных
-    # склеиваем два списка и отправляется в цикл по перебору всех страниц списка фильмов
-    # далее итоговый список всех фильмов перебираем и складываем в базу
+    """
+    Актуализация перечня фильмов.
+
+    Собираем сначала список новых фильмов(премьерных) + первая страница всех остальных
+    склеиваем два списка и отправляется в цикл по перебору всех страниц списка фильмов
+    далее итоговый список всех фильмов перебираем и складываем в базу
+    """
 
     url_new_movie_list = 'https://afisha.yandex.ru/api/events/actual?limit=12&offset=0&tag=cinema&hasMixed=0&' \
                          'filter=week-premiere&city=moscow'
@@ -130,23 +169,35 @@ def check_movie_in_db():
                           yandex_movie_id=movie['event']['id'],
                           title=movie['event']['title'],
                           start_date=movie['scheduleInfo']['dateReleased'],
-                          rating=str(movie['rank']))
+                          rating=movie['rank'])
     return movies_id
 
 
-def get_theater_id_by_yandex_id(yandex_theater_id):
+def get_theater_by_yandex_id(yandex_theater_id):
+    """
+    Получение инфо о кинотеатре по id кт с афиши яндекса.
+    """
     theater = db_session.query(MovieTheaters).filter_by(yandex_theater_id=yandex_theater_id).first()
     return theater if theater else None
 
 
-def get_movie_id_by_yandex_id(yandex_movie_id):
+def get_movie_by_yandex_id(yandex_movie_id):
+    """
+    Получение инфо о фильме по id фильма с афиши яндекса.
+    """
     movie = db_session.query(Movies).filter_by(yandex_movie_id=yandex_movie_id).first()
     return movie if movie else None
 
 
 def check_time_slot_in_db(movies_id):
+    """
+    Актуализация сеансов.
+
+    Получаем по id фильма его id на сайте афиши яндекса и формируем ссылку.
+    Достаем рассписание сеансов с постраничной навигацией(перебираем в цикле) и складывам в базу.
+    """
     for yandex_movie_id in list(set(movies_id)):
-        movie = get_movie_id_by_yandex_id(yandex_movie_id)
+        movie = get_movie_by_yandex_id(yandex_movie_id)
         print('Собираем расписание по фильму - {}'.format(movie.title))
         for counter in range(ITERATIONS):
             session_date = date.today() + timedelta(counter)
@@ -172,7 +223,7 @@ def check_time_slot_in_db(movies_id):
             for time_slot in time_slot_list:
                 theater_id_from_json = time_slot['place']['id']
                 # print(json.dumps(time_slot, sort_keys=True, indent=4, ensure_ascii=False))
-                theater = get_theater_id_by_yandex_id(theater_id_from_json)
+                theater = get_theater_by_yandex_id(theater_id_from_json)
 
                 # условие на случай если кинотеатра не будет в перечне кт(такое редко, но бывает)
                 if theater is None:
