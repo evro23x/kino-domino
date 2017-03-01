fba3043a0be36d8abb75663752ce57d0cd2ffb95
Evgeny Gurov <evro23x@mail.ru>
2017-03-01 21:54:22 +0300

    refactoring parser and tests

--- tests/test_parser_yandex.py
+++ tests/test_parser_yandex.py
@@ -1,13 +1,24 @@
 # import pytest
 
-# from parser_yandex import *
+from parser_yandex import *
 
 
 class TestParserYandex:
-    pass
-    # def test_get_metro_stations_from_hh_api_type(self):
-    #     assert (str(type(get_metro_stations_from_hh_api())) == str("<class 'list'>"))
+    # pass
+    def test_get_metro_stations_from_hh_api_type(self):
+        assert (str(type(get_metro_stations_from_hh_api())) == str("<class 'list'>"))
 
+    def test_get_metro_stations_from_hh_api_not_empty(self):
+        assert (len(get_metro_stations_from_hh_api()) > 0)
+
+    def test_add_metro_stations_type(self):
+        isinstance(add_metro_stations(get_metro_stations_from_hh_api()), str)
+        # assert (add_metro_stations(get_metro_stations_from_hh_api()[0]) is class)
+
+    # def test_add_metro_stations_not_empty(self):
+    #     assert (len(get_metro_stations_from_hh_api()) > 0)
+
+    #
     # def test_add_metro_stations(self):
     #     assert (str(type(add_metro_stations())) == str("<class 'list'>"))
 
