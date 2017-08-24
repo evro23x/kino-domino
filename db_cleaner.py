888537c4e6c97783258a9ea0349477dd2b7ed77d
EVGENY <evro23x@mail.ru>
2017-08-24 22:43:24 +0300

    fail with task duplicate in movie_theaters

--- db_cleaner.py
+++ db_cleaner.py
@@ -13,7 +13,13 @@ def get_duplicate_movie_theater_id():
     # Получаем id дубликатов, для последующего обновления таблицы Movies и удаления дубликатов из MovieTheaters
     for duplicate in duplicates:
         duplicate_ids = db_session.query(MovieTheaters.id).filter(MovieTheaters.title == duplicate[1]).all()
-        print(duplicate_ids)
+        # print(duplicate_ids)
+        # print(duplicate_ids[0][0])
+        # print(duplicate_ids[1][0])
+
+        print("First movie theater id = {} \nSecond movie theater id = {} ".format(duplicate_ids[0][0],
+              duplicate_ids[1][0]))
+
     exit(0)
 
     return duplicates
@@ -26,3 +32,11 @@ def update_movie_theater_id_in_movies_table():
 if __name__ == '__main__':
     mt_id = get_duplicate_movie_theater_id()
     # update_movie_theater_id_in_movies_table(mt_id)
+
+    # делаем выборку из таблицы movie_theaters по title если совпадений больше 1
+    # разбираем получившийся массив одномерных массивов по нулевому ключу
+    # обновляем таблицу time_slots, в поле movie_theaters_id меняем на второе значение из массива дублей
+    # удаляем кт из первого списка
+    # удаляем кт по id из таблицы movie_theaters
+
+    # !!! Обязательно доделать это, очень важный и критический таск. !!!
